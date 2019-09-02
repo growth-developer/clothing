a013f462233d61abc4e6140312d4abf1ae14499e
growth-developer <hello.growthdeveloper@gmail.com>
2019-09-02 17:16:45 +0200

    update our menu items to navigate withRouter

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
+
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
+ReactDOM.render(
+    // BrowerRouter is a component; it gives all functionality to of routing to <App /> component
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>, 
+    document.getElementById('root'));
